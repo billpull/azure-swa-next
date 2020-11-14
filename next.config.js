2e1699eccc248655ff39377debd6dcd1adb07bee
Bill Pullen <bill.pull@gmail.com>
2020-11-14 17:54:54 -0500

    pwa test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-module.exports = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
     trailingSlash: true,
     exportPathMap: function() {
       return {
         '/': { page: '/' },
         '/hello': { page: '/hello' }
       };
-    }
-  };
+    },
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+    },
+})
